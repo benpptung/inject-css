4683aea8ef2410d11a8af6d2ae1590773991bba5
benpptung <benpptung@tacol.biz>
2018-04-02 12:26:48 +0800

    use eslint

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-'use strict';
+'use strict'
 
-var inserted = {};
+var inserted = {}
 
 module.exports = function(css, options) {
 
-  if (inserted[css]) return;
-  inserted[css] = true;
+  if (inserted[css]) return removeCss
+  inserted[css] = true
 
-  var elm = document.createElement('style');
-  elm.setAttribute('type', 'text/css');
+  var elm = document.createElement('style')
+  elm.setAttribute('type', 'text/css')
 
   if ('textContent' in elm) {
-    elm.textContent = css;
+    elm.textContent = css
   }
   else {
-    elm.styleSheet.cssText = css;
+    elm.styleSheet.cssText = css
   }
 
-  var head = document.getElementsByTagName('head')[0];
+  var head = document.getElementsByTagName('head')[0]
   if (options && options.prepend) {
-    head.insertBefore(elm, head.childNodes[0]);
-  } else {
-    head.appendChild(elm);
+    head.insertBefore(elm, head.childNodes[0])
+  }
+  else {
+    head.appendChild(elm)
   }
 
-  return function() {
-    delete inserted[css];
-    head.removeChild(elm);
-  };
-};
+  function removeCss() {
+    delete inserted[css]
+    head.removeChild(elm)
+  }
+}
