0debdc85acfaf93a83e337784e6bdc5845596b5e
benpptung <benpptung@tacol.biz>
2018-05-04 15:27:18 +0800

    add session css handling in server side rendering

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 'use strict'
 
 var inserted = {}
+var session = {}
 var is_client = typeof window === 'object'
 
+/**
+ * insert css inside head tag. and return a function to remove css and cached
+ * @param  {string} css     css rules string
+ * @param  {object} options
+ * @return {function}       remove the style element and cached css
+ */
 exports = module.exports = function(css, options) {
+  return insert(inserted, css, options)
+}
+
+/**
+ * same as module.exports. This for server side rendering
+ * if called inside a session.
+ * @param  {string} css     css rules string
+ * @param  {object} options
+ * @return {function}       remove the style element and cached css
+ */
+exports.session = function(css, options) {
+  return insert(session, css, options)
+}
+
+/**
+ * return css strings in array
+ * @return {array}
+ */
+exports.getCss = getCss
+function getCss() {
+  return Object.keys(inserted).concat(Object.keys(session))
+}
+exports.cleanAllCss = function() {
+  cleanStore(inserted)
+  cleanStore(session)
+}
+
+exports.getCssAndResetSess = function() {
+  var css = getCss()
+  cleanStore(session)
+  return css
+}
+exports.cleanSessCss = function() {
+  cleanStore(session)
+}
+
+function cleanStore(store) {
+  var arr = Object.keys(store)
+  for (var i = 0, len = arr.length; i < len; ++i) {
+    var fn = store[arr[i]]
+    delete store[arr[i]]
+    fn()
+  }
+}
+function insert(store, css, options) {
 
   if (!css) return nop
-  if (inserted[css]) return nop
-
-  inserted[css] = true
+  if (store[css]) return store[css]
+  store[css] = removeCss
 
   var elm = null
   var head = null
@@ -36,18 +87,9 @@ exports = module.exports = function(css, options) {
   return removeCss
 
   function removeCss() {
-    delete inserted[css]
-
+    delete store[css]
     if (!is_client) return
     head.removeChild(elm)
   }
 }
-
-exports.getCachedCss = function() {
-  return Object.keys(inserted)
-}
-exports.delCachedCss = function() {
-  inserted = {}
-}
-
 function nop(){ }
